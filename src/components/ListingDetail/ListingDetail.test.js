958a48a333fe1ab4ed7100bd3a61245f3cf2148a
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-04 12:50:11 -0700

    Boilerplate and Image import

---
+++ src/components/ListingDetail/ListingDetail.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ListingDetail from './ListingDetail';
+
+describe('ListingDetail', () => {
+  it('', () => {
+    
+  })
+})
---
+++ src/components/Login/Login.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import './Login.scss';
+
+export default Login = () => {
+  return ()
+}
