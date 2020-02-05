76b33c7751f8bd620c54f325600f3f9e77e15975
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-04 18:29:59 -0700

    Creates handler to set state and login user

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.scss';
 
-const Login = () => {
-  return (
-    <form className="login-form">
-      <h1>Dwell-n-Dine</h1>
-      <label htmlFor="login-name">Name</label>
-      <input id="login-name" placeholder="Name"></input>
-      <label htmlFor="login-email">Email</label>
-      <input id="login-email" placeholder="Email"></input>
-      <label htmlFor="login-reason">Reason for visit</label>
-      <select id="login-reason" name="reason">
-        <option value="Special Occasion">Special Occasion</option>
-        <option value="Business">Business</option>
-        <option value="Vacation">Vacation</option>
-        <option value="Other">Other</option>
-      </select>
-      <button className="login-button">Login</button>
-    </form>
-  );
-};
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      reason: ''
+    };
+  }
 
-export default Login;
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  loginUser = e => {
+    e.preventDefault();
+    const { setUserInfo } = this.props;
+    setUserInfo({ ...this.state });
+  };
+
+  render() {
+    return (
+      <form className="login-form">
+        <h1>Dwell-n-Dine</h1>
+        <label htmlFor="login-name">Name</label>
+        <input
+          onChange={this.handleChange}
+          name="name"
+          id="login-name"
+          placeholder="Name"
+        ></input>
+        <label htmlFor="login-email">Email</label>
+        <input
+          onChange={this.handleChange}
+          name="email"
+          id="login-email"
+          placeholder="Email"
+        ></input>
+        <label htmlFor="login-reason">Reason for visit</label>
+        <select onChange={this.handleChange} id="login-reason" name="reason">
+          <option disabled selected value>
+            -- select an option --
+          </option>
+          <option value="Special Occasion">Special Occasion</option>
+          <option value="Business">Business</option>
+          <option value="Vacation">Vacation</option>
+          <option value="Other">Other</option>
+        </select>
+        <button onClick={this.loginUser} className="login-button">
+          Login
+        </button>
+      </form>
+    );
+  }
+}
