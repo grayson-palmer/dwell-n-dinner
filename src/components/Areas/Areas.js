f41acca0ecb9b9f141fff258a49d44dbefdde218
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-07 20:52:58 -0700

    Modify props to read link path

--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Areas = props => {
   const areaCards = props.areaSpecs.map(area => {
     return (
-      <Link className="area-card">
+      <Link to={`/areas/${area.id}`} className="area-card" onClick={() => props.setAreaId(area.id)}>
         <h2>{area.name} - <i>{area.area}</i></h2>
         <p>{area.location}</p>
         <p>{area.about}</p>
