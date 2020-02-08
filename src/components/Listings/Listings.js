1e4eacea687bcab8090d41018e518e760a5f68ec
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-07 20:53:57 -0700

    Populate listings onto DOM

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,6 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Listings.scss';
 
-export default Listings = () => {
-  return ()
+const Listings = (props) => {
+  const filteredListings = props.listings.filter(listing => listing.area_id === props.selectedAreaId)
+  const domListings = filteredListings.map(listing => {
+    return (
+      <div className='listing-card'>
+        <h4>{listing.name}</h4>
+        <img src={`/images/${listing.listing_id}_a.jpg`} alt={listing.name} width='100px'/>
+        <button type='button'>See Details</button>
+      </div>
+    )
+  })
+
+  
+
+  return (
+    <section className='listing-section'>
+      {domListings}
+    </section>
+    )
 }
+
+export default Listings
