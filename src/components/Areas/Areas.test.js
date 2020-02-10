9c833e7fa3a475f3ecfecdf4dd5ce6aa3d94549c
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-10 16:40:56 -0700

    Write test for Areas

--- src/components/Areas/Areas.test.js
+++ src/components/Areas/Areas.test.js
@@ -3,7 +3,19 @@ import { shallow } from 'enzyme';
 import Areas from './Areas';
 
 describe('Areas', () => {
-  it('', () => {
-    
-  })
-})
+  it('should render the component', () => {
+    const wrapper = shallow(
+      <Areas
+        areaDetails={[
+          {
+            name: 'River North',
+            area: 'RiNo',
+            location: 'location',
+            about: 'blah, blah, blah'
+          }
+        ]}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
