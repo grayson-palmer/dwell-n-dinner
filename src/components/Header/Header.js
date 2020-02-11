5f917a7fa7249ab49aad7094bc5043e5e96b2c06
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-11 15:11:04 -0700

    Show user info in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
-const Header = ({ user }) => {
+const Header = ({ user, setUserInfo }) => {
   return (
     <header className="header">
       <ul className="header__user-info">
-        <li>username</li>
-        <li>email</li>
-        <li>reason</li>
+        <li>{user.name}</li>
+        <li>{user.email}</li>
+        <li>{user.reason}</li>
       </ul>
       <h1 className="header__title">Dwell-n-Dine</h1>
       <div className="header__nav">
@@ -16,7 +16,11 @@ const Header = ({ user }) => {
           <img src="../../images/favorite-icon.png" alt="favorite icon" />
           <div>Favorites</div>
         </NavLink>
-        <NavLink to="path-to-favorites" className="header__button log-out">
+        <NavLink
+          onClick={setUserInfo}
+          to="path-to-favorites"
+          className="header__button log-out"
+        >
           <img src="../../images/logout-icon.png" alt="logout icon" />
           <div>Log Out</div>
         </NavLink>
